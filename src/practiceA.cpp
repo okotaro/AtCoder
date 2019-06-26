72de5e72b585eb64c654c57bab7f313f00ae5ad0
okotaro <goutape@gmail.com>
2019-06-26 18:37:54 +0900

    Add a practice code

---
+++ src/practiceA.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+  int a;
+  std::cin >> a;
+  int b, c;
+  std::cin >> b >> c;
+  
+  int sum = a + b + c;
+  
+  std::string str;
+  std::cin >> str;
+  
+  std::cout << sum << " " << str << std::endl;
+  return 0;
+}
